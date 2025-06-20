5028ee418b8e7374e38682915e7e8364dab2f482
Mahishamardini11 <mahishamardinisekar@gmail.com>
2025-06-20 18:10:13 +0530

    Added css and js api and tested

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+//api key --- d9346758897023a639bc13a02ce814f3
+//api url --- https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
+const apiKey = "d9346758897023a639bc13a02ce814f3";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore"
+
+async function checkWeather(){
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    var data = await response.json();
+    console.log(data);
+}
+checkWeather();
